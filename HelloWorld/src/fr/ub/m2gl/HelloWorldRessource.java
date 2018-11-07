23fb9b90a62e77815865e4948c4dd274c3325b80
Probak <probakilla@gmail.com>
2018-11-07 08:29:31 +0100

    Ajout d'un user à la bdd

--- HelloWorld/src/fr/ub/m2gl/HelloWorldRessource.java
+++ HelloWorld/src/fr/ub/m2gl/HelloWorldRessource.java
@@ -1,13 +1,20 @@
 package fr.ub.m2gl;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @Path("/")
 public class HelloWorldRessource {
@@ -23,7 +30,24 @@ public class HelloWorldRessource {
     @Path("/register")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response registerUser(User user) {
-    	String out =  user.toString();
+    	String out =  "";
+		
+	    MongoClientURI connectionString = new MongoClientURI("mongodb://jacky:mdp1234@ds063870.mlab.com:63870/progweb");
+	    MongoClient client = new MongoClient(connectionString);
+		try {
+		    MongoDatabase db = client.getDatabase("progweb");
+		    MongoCollection<Document> collection = db.getCollection("users");
+		    ObjectMapper mapper = new ObjectMapper ();
+		    String jsonString = mapper.writeValueAsString(user);
+		    Document doc = Document.parse(jsonString);
+		    collection.insertOne(doc);
+		    out = "INSERSION SUCESS";
+		} catch (Exception e) {
+		    out = "INSERSION FAILED";
+		    e.printStackTrace();
+		} finally {
+		    client.close();
+		}
     	return Response.status(200).entity(out).build();
-    }
+    }   
 }
